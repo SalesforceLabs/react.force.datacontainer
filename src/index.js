f1283c2cc507cd09bf9c8fe58712f17af0b3caf7
ibogdanov <ibogdanov@salesforce.com>
2016-06-03 00:13:28 -0700

    export util

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import SobjContainer from './SobjContainer';
 import List from './List';
 import RelevantItems from './RelevantItems';
+import {util} from 'react.force.data';
 
 module.exports = {
   SobjContainer: SobjContainer,
   Sobj: SobjContainer,
   ListContainer: List,
-  RelevantItems: RelevantItems
+  List: List,
+  RelevantItems: RelevantItems,
+  util: util
 };
